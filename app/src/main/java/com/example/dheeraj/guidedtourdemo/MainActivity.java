1c126515382c53b2aa550ba3bec49eb0d4db08ad
dheeraj <dheeraj.sachan@aurusnet.com>
2015-07-21 12:39:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dheeraj/guidedtourdemo/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.dheeraj.guidedtourdemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.targets.ActionViewTarget;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.test);
+        Button button1 = (Button) findViewById(R.id.test1);
+
+
+        new ShowcaseView.Builder(this)
+                .setTarget(new ViewTarget(((ViewGroup) findViewById(R.id.action_bar)).getChildAt(0) /*button*/))
+                .setContentTitle("ShowcaseView")
+                .setContentText("This is highlighting the Home button")
+                .hideOnTouchOutside().setStyle(R.style.CustomShowcaseTheme)
+                .build().setPadding(0,0,0,100);
+
+ /*       new ShowcaseView.Builder(this)
+                .setTarget( new ViewTarget( *//*((ViewGroup)findViewById(R.id.action_bar)).getChildAt(0)*//* button1 ) )
+                .setContentTitle("ShowcaseView1")
+                .setContentText("This is highlighting the Home button1")
+                .hideOnTouchOutside()
+                .build();*/
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"button1 clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+
+/*        new ShowcaseView.Builder(this)
+                .setTarget(new ActionViewTarget(this, ActionViewTarget.Type.HOME))
+                .setContentTitle("ShowcaseView")
+                .setContentText("This is highlighting the Home button")
+                .hideOnTouchOutside()
+                .build();*/
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
